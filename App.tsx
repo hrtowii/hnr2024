14caa29de5c33815479bdae5ac1516a9dfa5b32d
hrtowii <68852354+hrtowii@users.noreply.github.com>
2024-01-20 14:08:56 +0800

    a

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Redirect} from "expo-router";
+import React from 'react';
 
 export default function App() {
   return (
